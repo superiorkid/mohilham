fa936342b2bc6f9d004170370f549fb3a2d1b202
superiorkid <bayiPetani@protonmail.com>
2023-04-06 15:14:53 +0800

    new footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,13 +2,23 @@ import Link from "next/link";
 import React from "react";
 import { FiMail } from "react-icons/fi";
 import { AiOutlineHeart } from "react-icons/ai";
-import { FaRss, FaLinkedin, FaGithub, FaTwitter } from "react-icons/fa";
+import {
+  FaRss,
+  FaLinkedin,
+  FaGithub,
+  FaTwitter,
+  FaCopyright,
+  FaHeart,
+} from "react-icons/fa";
 
 function Footer() {
   return (
     <div className="justify-center md:justify-start flex flex-row py-5 md:pb-5 md:pt-14 px-5">
       <div className="flex flex-row space-x-11 md:space-x-5">
-        <Link
+        <span className="text-md text-neutral-600 flex items-center">
+          Made with <FaHeart className="mx-1 text-red-500" /> by me
+        </span>
+        {/* <Link
           href="#subscribe"
           className="flex flex-row items-center space-x-1"
         >
@@ -28,7 +38,7 @@ function Footer() {
         >
           <FaRss />
           <span>RSS</span>
-        </Link>
+        </Link> */}
       </div>
     </div>
   );
