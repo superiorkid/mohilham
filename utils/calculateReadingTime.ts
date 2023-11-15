113deb54926fadf136b40df33f7ec747e1f05b0c
superiorkid <bayiPetani@protonmail.com>
2023-11-15 18:47:24 +0800

    better loading ui in project page

--- utils/calculateReadingTime.ts
+++
@@ -1,6 +0,0 @@
-export default function calculateReadingTime(body: string) {
-  const wordTotal: number = body.split(" ").filter((num) => {
-    return num != "";
-  }).length;
-  return wordTotal / 200;
-}
